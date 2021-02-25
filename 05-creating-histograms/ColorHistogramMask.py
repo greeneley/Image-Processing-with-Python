bc8707981ae47ad26cbaf21c8fff96e10b970722
haidt27 <haidt27@viettel.com.vn>
2021-02-25 17:27:21 +0700

    upload solution for chapter 5

--- 05-creating-histograms/ColorHistogramMask.py
+++ 05-creating-histograms/ColorHistogramMask.py
@@ -20,16 +20,21 @@ viewer.show()
 
 # create a circular mask to select the 7th well in the first row
 # WRITE YOUR CODE HERE
-
+mask = np.zeros(shape=image.shape[0:2], dtype="bool")
+circle = skimage.draw.circle(240, 1053, radius=49, shape=image.shape[:2])
+mask[circle] = 1
 # just for display:
 # make a copy of the image, call it masked_image, and
 # use np.logical_not() and indexing to apply the mask to it
 # WRITE YOUR CODE HERE
 
+masked_img = image.copy()
+masked_img[np.logical_not(mask)] = 0
 # create a new window and display masked_image, to verify the
 # validity of your mask
 # WRITE YOUR CODE HERE
-
+viewer = skimage.viewer.ImageViewer(masked_img)
+viewer.show()
 # list to select colors of each channel line
 colors = ("r", "g", "b")
 channel_ids = (0, 1, 2)
@@ -41,8 +46,7 @@ for channel_id, c in zip(channel_ids, colors):
     # change this to use your circular mask to apply the histogram
     # operation to the 7th well of the first row
     # MODIFY CODE HERE
-    histogram, bin_edges = np.histogram(image[:, :, channel_id], bins=256, range=(0, 256))
-
+    histogram, bin_edges = np.histogram(image[:, :, channel_id][mask], bins=256, range=(0, 256))
     plt.plot(bin_edges[0:-1], histogram, color=c)
 
 plt.xlabel("color value")
--- 05-creating-histograms/GrayscaleMaskHistogram.py
+++ 05-creating-histograms/GrayscaleMaskHistogram.py
@@ -12,7 +12,7 @@ from matplotlib import pyplot as plt
 # read image, based on command line filename argument;
 # read the image as grayscale from the outset
 image = skimage.io.imread(fname=sys.argv[1], as_gray=True)
-
+# image = skimage.io.imread(fname="seedling.jpg", as_gray=True)
 # display the image
 viewer = skimage.viewer.ImageViewer(image)
 viewer.show()
@@ -20,9 +20,13 @@ viewer.show()
 # create mask here, using np.zeros() and skimage.draw.rectangle()
 # WRITE YOUR CODE HERE
 
+mask = np.zeros(shape=(image.shape),dtype="uint8")
+rr, cc = skimage.draw.rectangle(start=(206,378),end=(594,545))
+mask[rr,cc] = False
 # apply the mask to the input image, create the histogram
 # MODIFY CODE HERE
-histogram, bin_edges = np.histogram(image, bins=256, range=(0.0, 1.0))
+histogram, bin_edges = np.histogram(image[mask], bins=256, range=(0.0, 1.0))
+
 
 # configure and draw the histogram figure
 plt.figure()
